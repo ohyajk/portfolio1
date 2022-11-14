09bf3f7f7d3afcb159ad7a0ace1e2df14584aff3
Jitender Kumar <ohyajk@gmail.com>
2022-11-14 19:35:43 +0530

    Update script Js file

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
-function openMenu() {
-    document.getElementById("menu").style.display="block";
-}
-
-function closeMenu() {
-    document.getElementById("menu").style.display="none";
-}
-
+const ham = document.getElementById('hamburger');
+const cross = document.getElementById('cross');
+const ul = document.getElementById('ul');
+function openMenu() { ul.style.display = 'flex'; }
+function closeMenu() { ul.style.display = 'none'; }
+cross.addEventListener('click', closeMenu);
+ul.addEventListener('click', closeMenu);
+ham.addEventListener('click', openMenu);
